aec1d1d70f36640890ff8503682cd16d2141b898
Logon Aluno <logonaluno@local.com>
2019-12-14 12:49:16 -0300

    v2

--- Calculadora/app/src/main/java/br/com/arthurcoutinho/calculadora/ui/signup/SignupActivity.kt
+++ Calculadora/app/src/main/java/br/com/arthurcoutinho/calculadora/ui/signup/SignupActivity.kt
@@ -2,10 +2,12 @@ package br.com.arthurcoutinho.calculadora.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.arthurcoutinho.calculadora.R
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
     }
 }
